e8ade89ef066b1082c82ec5ad1fc947751cd8599
Meylin Leonario <meylin.leonario@alumnos.ucn.cl>
2024-05-28 20:54:50 -0400

    Todas las clases creadas

--- Clases/Comentario.java
+++ Clases/Comentario.java
@@ -1,5 +1,12 @@
 package Clases;
 
 public class Comentario {
+    private String comment;
+    private double rating;
+
+    public Comentario(String comment, double rating) {
+        this.comment = comment;
+        this.rating = rating;
+    }
     
 }
--- Clases/Comments.java
+++ Clases/Comments.java
@@ -1,7 +1,27 @@
 package Clases;
 
+import java.util.List;
+
 public class Comments {
     private String isbn;
     private int cantidad_comentarios;
     private List<Comentario> comentarios;
+
+    public Comments(String isbn, int cantidad_comentarios, List<Comentario> comentarios) {
+        this.isbn = isbn;
+        this.cantidad_comentarios = cantidad_comentarios;
+        this.comentarios = comentarios;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getCantidad_comentarios() {
+        return cantidad_comentarios;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
 }
