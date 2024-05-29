9dc0534a420a78fbc9ddfb4a749d251f67964a6d
Meylin Leonario <meylin.leonario@alumnos.ucn.cl>
2024-05-29 07:30:41 -0400

    In in new in

--- Sistema/SistemaImpl.java
+++ Sistema/SistemaImpl.java
@@ -1,12 +1,16 @@
 package Sistema;
 
+
+import edu.princeton.cs.stdlib.In;
+
 public class SistemaImpl implements Sistema{
     public void lecturaDeArchivos(){
         //HOLA
     }
 
     public void usuarioLectura(){
-        
+        In in = new In();
+
     }
 
     public void menu(){
