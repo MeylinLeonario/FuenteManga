509f457172b282d70f38690bf873994d70e92b14
Gimena3 <gimena.zavaleta@alumnos.ucn.cl>
2024-05-31 11:21:05 -0400

    menu
    menu primera parte administrador

--- Sistema/SistemaImpl.java
+++ Sistema/SistemaImpl.java
@@ -1,7 +1,11 @@
 package Sistema;
 
 
+import Clases.Administrador;
+import Clases.Rol;
 import edu.princeton.cs.stdlib.In;
+
+import java.util.Scanner;
 
 public class SistemaImpl implements Sistema{
     public void lecturaDeArchivos(){
@@ -13,9 +17,33 @@ public class SistemaImpl implements Sistema{
 
     }
 
-    public void menu(){
-        System.out.println("Hola");
-    }
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("<------{Inicio de Sesión}------>");
+        System.out.println("1. Administrador");
+        System.out.println("2. Usuario");
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
 
+        if (opcion == 1) {
+            System.out.println("<------{Inicio de Sesión}------>");
+            System.out.println("Inicio de sesión como administrador");
+            System.out.println("Id-Administrador:");
+            String administradorId = scanner.nextLine();
+            System.out.println("Nombre de Usuario: ");
+            String nombreUsuario = scanner.nextLine();
+            System.out.println("Contraseña:");
+            String password = scanner.nextLine();
+            Rol rol = Rol.ADMINISTRADOR;
+            int id = Integer.parseInt(administradorId);
+            Administrador administrador = new Administrador(rol, nombreUsuario, id, password, administradorId);
+            System.out.println("Bienvenido, " + administradorId);
+
+        } else if (opcion == 2) {
+            System.out.println("<------{Inicio de Sesión}------>");
+            System.out.println("Inicio de sesión como usuario");
+
+        }
+    }
 
 }
